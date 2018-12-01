dd0076405a2a84f3b623ba9a28ec5eb60b7bd191
fraggerfox <santhosh.raju@gmail.com>
2018-12-01 15:12:59 +0530

    Fixed an issue with directory listing not appearing due to failues in iconv_open() call.

--- audio/ft2clone/patches/patch-src_ft2__unicode.c
+++ audio/ft2clone/patches/patch-src_ft2__unicode.c
@@ -1,9 +1,12 @@
 $NetBSD$
 
-Added a type cast to iconv(3) calls to match the prototypes defined
+1. Added a type cast to iconv(3) calls to match the prototypes defined
 in NetBSD's iconv.h.
 
---- src/ft2_unicode.c.orig	2018-07-01 14:43:52.000000000 +0000
+2. Added a NetBSD specific iconv_open() call to prevent failures due
+to mismatches iconv_open() parameters present in GNU iconv(3).
+
+--- src/ft2_unicode.c.orig	2018-07-01 09:13:52.000000000 +0000
 +++ src/ft2_unicode.c
 @@ -275,7 +275,11 @@ char *cp437ToUtf8(char *src)
      inLen  = srcLen;
@@ -17,7 +20,16 @@ in NetBSD's iconv.h.
      iconv(cd, NULL, NULL, &outPtr, &outLen); /* flush */
      iconv_close(cd);
  
-@@ -323,7 +327,11 @@ char *utf8ToCp437(char *src, uint8_t rem
+@@ -307,6 +311,8 @@ char *utf8ToCp437(char *src, uint8_t rem
+     */
+ #ifdef __APPLE__
+     cd = iconv_open("437//TRANSLIT//IGNORE", "UTF-8-MAC");
++#elif defined(__NetBSD__)
++    cd = iconv_open("437", "UTF-8");    
+ #else
+     cd = iconv_open("437//TRANSLIT//IGNORE", "UTF-8");
+ #endif
+@@ -323,7 +329,11 @@ char *utf8ToCp437(char *src, uint8_t rem
      inLen  = srcLen;
      outPtr = outBuf;
  
